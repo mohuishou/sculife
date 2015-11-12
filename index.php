57ef69549d51fde539301488fb4ebe5c53be02aa
mohuishou <1@lailin.xyz>
2015-11-12 17:45:08 +0800

    重构目录，删除一些不必要的文件

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-=======
+
 // 应用入口文件
->>>>>>> d338b48a60e35ef738596499a352cf0c50d10d0c
 
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
@@ -12,16 +10,8 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',True);
 
 // 定义应用目录
-<<<<<<< HEAD
 define('APP_PATH','./admin/');
-=======
-define('APP_PATH','./Application/');
->>>>>>> d338b48a60e35ef738596499a352cf0c50d10d0c
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
-<<<<<<< HEAD
-// 亲^_^ 后面不需要任何代码了 就是如此简单
-=======
->>>>>>> d338b48a60e35ef738596499a352cf0c50d10d0c
